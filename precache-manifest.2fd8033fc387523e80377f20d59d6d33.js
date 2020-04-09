6fda128af46c91ef4f185b905cf4828b6c96c49f
misaelalamillo <misaelalamillo@gmail.com>
2020-04-09 11:54:00 -0500

    Updates

---
+++ precache-manifest.2fd8033fc387523e80377f20d59d6d33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f336cab33767613d4719b107481ad75e",
+    "url": "/miso-portfolio/index.html"
+  },
+  {
+    "revision": "b0daeba519b821c22215",
+    "url": "/miso-portfolio/static/css/main.bded3a80.chunk.css"
+  },
+  {
+    "revision": "0b1f938262b31a981587",
+    "url": "/miso-portfolio/static/js/2.9d087cbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/miso-portfolio/static/js/2.9d087cbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0daeba519b821c22215",
+    "url": "/miso-portfolio/static/js/main.af2df952.chunk.js"
+  },
+  {
+    "revision": "c16343bf9dec1cda95af",
+    "url": "/miso-portfolio/static/js/runtime-main.132b2e8e.js"
+  }
+]);
